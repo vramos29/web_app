b85f835330325311efd79fa3aab1a96fce2a34c3
vramos29 <vramos1@northpark.edu>
2026-02-13 11:47:42 -0600

    fixed count to 1

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import requests
 URL1 = "https://geocoding-api.open-meteo.com/v1/search"
 URL2 = "https://api.open-meteo.com/v1/forecast?current_weather=true"
 
-params = {"name": "Chicago", "country": "United States", "count": 4}
+params = {"name": "Chicago", "country": "United States", "count": 1}
 response = requests.get(URL1, params=params, timeout = 12)
 print("Status:", response.status_code)                                    
-print("Reponse:",response.json()[:0]) #only accepts without slice (.json())
+print("Reponse:",response.json()) 
 
