8f6b06dd47e5c039f25577fd14a642e322ff88f3
vramos29 <vramos1@northpark.edu>
2026-02-17 14:01:18 -0600

    second attempt at retrieving lat and long... unsuccessful

--- main.py
+++ main.py
@@ -7,17 +7,17 @@ URL1 = "https://geocoding-api.open-meteo.com/v1/search"
 URL2 = "https://api.open-meteo.com/v1/forecast?current_weather=true"
 
 params = {"name": "Chicago", "country": "United States", "count": 1}
-response = requests.get(URL1, params=params, timeout = 12)
+response = requests.get(URL1, params=params, timeout = 7)
 response.raise_for_status()
 
+data = response.json() 
+print(data)         #works / prints out full json string
 
-data = response.json()
-print(data)
+longitude = data.get('longitude')
+latitude = data.get('latitude')
 
-latitude = data['latitude']     #not working
-longitude = data['longitude']
+print(f"{longitude}, {latitude}")   #returns None, None
 
-print(f"{longitude}, {latitude}")
 
 
 
