15ca2778d760b2437b0869b300236871a0989f40
vramos29 <vramos1@northpark.edu>
2026-02-17 13:14:35 -0600

    tried to retrieve longitude and latitude...unsuccessful

--- main.py
+++ main.py
@@ -2,11 +2,25 @@
 
 import requests
 
+
 URL1 = "https://geocoding-api.open-meteo.com/v1/search"
 URL2 = "https://api.open-meteo.com/v1/forecast?current_weather=true"
 
 params = {"name": "Chicago", "country": "United States", "count": 1}
 response = requests.get(URL1, params=params, timeout = 12)
-print("Status:", response.status_code)                                    
-print("Reponse:",response.json()) 
+response.raise_for_status()
+
+
+data = response.json()
+print(data)
+
+latitude = data['latitude']     #not working
+longitude = data['longitude']
+
+print(f"{longitude}, {latitude}")
+
+
+
+
+
 
