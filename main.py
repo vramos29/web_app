c6e8337028353ad1327cc758fece3dd07222a80f
vramos29 <vramos1@northpark.edu>
2026-02-18 20:01:43 -0600

    Completed API requests, successfully converted to python object

--- main.py
+++ main.py
@@ -7,17 +7,30 @@ URL1 = "https://geocoding-api.open-meteo.com/v1/search"
 params = {"name": "Chicago", "country": "United States", "count": 1}
 response = requests.get(URL1, params=params, timeout = 7)
 response.raise_for_status()
- 
-geo_data = response.json() 
 
-results = geo_data['results']           
+
+def check_geo_data():
+    geo_data = response.json() 
+
+    if 'results' not in geo_data:
+        print("No 'results' found.")
+    global results
+    results = geo_data['results']
+
+    if 'longitude' not in results or not isinstance(results['longitude'], int):
+        return("'Longitude' not found or has incorrect value")
+    if 'latitude' not in results or not isinstance(results['latitude'], int):
+        return("'Latitude' not found or has incorrect value")
+    if 'city' not in results or not isinstance(results['name'], str):
+        return("'City' not found or has incorrect value")
+    if 'country_code' not in results or not isinstance(results['country_code'], str):
+        return("'Country' not found or has incorrect value")
+
+check_geo_data()
 longitude = results[0]['longitude'] 
 latitude = results[0]['latitude']
 city = results[0]['name']
 country = results[0]['country_code']
-
-print(response)
-
 
 
 
@@ -27,47 +40,65 @@ params2 = {"latitude": latitude, "longitude": longitude}
 response2 = requests.get(URL2, params=params2, timeout = 7)
 response2.raise_for_status()
 
-meteo_data = response2.json()
 
-print(meteo_data)
+def check_meteo_data():
+    global meteo_data
+    meteo_data = response2.json()
 
+    if 'current_weather' not in meteo_data:
+        print("No 'results' found.")
+    global current_weather
+    current_weather = meteo_data['current_weather']
+
+    if 'temp' not in current_weather or not isinstance(current_weather['temperature'], int):
+        return("'Temperature' not found or has incorrect value")
+    if 'elevation' not in meteo_data or not isinstance(meteo_data['relevation'], int):
+        return("'Elevation' not found or has incorrect value")
+    if 'windspeed' not in current_weather or not isinstance(current_weather['windspeed'], int):
+        return("'City' not found or has incorrect value")
+    if 'observation_time' not in current_weather:
+        return("'Observation time' not located")
+
+check_meteo_data()
 current_weather = meteo_data['current_weather']
-temperature = current_weather['temperature']
+temp = current_weather['temperature']
 elevation = meteo_data['elevation']
 windspeed = current_weather['windspeed']
 observation_time = current_weather['time']
 
 
+data = {
+    "City": city,
+    "Country": country,
+    "Latitude": f"{latitude}°",
+    "Longitude": f"{longitude}°",
+    "Temperature": f"{temp}°C",
+    "Elevation": f"{elevation}",
+    "Windspeed": f"{windspeed} km/h",
+    "Observation Time": observation_time
+}
+
+print(f" Weather Report Data: \n{data}")
+
 
 #Turns data into python object
 
-report = {}
+class WeatherReport:
+    city: str
+    country: str
+    latitude: float
+    longitude: float
+    temp: float
+    elevation: float
+    windspeed: float
+    observation_time
 
-class Weather_Info:
-    def __init__(self, city, country, latitude, longitude, temperature, elevation, windspeed, observation_time):
-        self.city = city
-        self.country = country
-        self.latitude = latitude
-        self.longitude = longitude
-        self.temperature = temperature
-        self.elevation = elevation
-        self.windspeed = windspeed
-        self.observation_time = observation_time
-    
-    def form_report(self):
-        global report
-        report = {
-            "City": self.city,
-            "Country": self.country,
-            "Latitude": f"{self.latitude}°",
-            "Longitude": f"{self.longitude}°",
-            "Temperature": f"{self.temperature}°C",
-            "Elevation": self.elevation,
-            "Windspeed": f"{self.windspeed} km/h",
-            "Obersation Time": self.observation_time
-            }
-        return report
-    
+report = WeatherReport()
+print(f"WeatherReport object: {report}")
+
+        
+
+
 
         
 
