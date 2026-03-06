3341d53c1dad83adc247464bb67d5e4ba570fe06
vramos29 <vramos1@northpark.edu>
2026-03-05 19:10:41 -0600

    completed basic Flask routes and updated requirements.txt

--- app.py
+++ app.py
@@ -14,22 +14,27 @@ app = Flask(__name__)
 def home():  
     return '<h1>This is the weather report app starter page</h1>'
 
-
 #This will be the post route that allows the user to enter the location for the weather report
 @app.route('/main')
 def enter_request():
-    return None
+    return '<h1>This is the main page</h1>'
 
 #After entering their request, this is the get route that will display the information requested
 @app.route('/report')
 def retrieve_report():
-    return None
+    return '<h1>This is where you can read the weather report(s)</h1>'
 
 #This route will allow the user to first view their reports, then update OR delete a report that has already been recorded and stored
-@app.route('/report-update')
+@app.route('/update-report')
 def update_report():
-    return None
+    return '<h1>This allows you to update </h1>'
+def delete_report():
+    return '<h1>and delete the reports as desired</h1>'
 
+#This will allow you to download a copy of the report(s)
+@app.route('/download-report')
+def download_report():
+    return '<h1>This will allow you to download a copy of the report</h1>'
 
 # This ensures the server only runs if the script is executed directly - returns TypeError.
 if __name__ == '__main__':
