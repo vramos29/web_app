a7ed3f14bff3b93451759beb33c658f9dd0d0896
vramos29 <vramos1@northpark.edu>
2026-03-06 18:48:17 -0600

    Adjusted the routes as needed, defined methods for more clear approach for each route

--- app.py
+++ app.py
@@ -15,21 +15,23 @@ def home():
     return '<h1>This is the weather report app starter page</h1>'
 
 #This will be the post route that allows the user to enter the location for the weather report
-@app.route('/main')
+@app.route('/weather', methods = ['POST'])
 def enter_request():
-    return '<h1>This is the main page</h1>'
+    return '<h1>This is where you can generate a weather report</h1>'
 
 #After entering their request, this is the get route that will display the information requested
-@app.route('/report')
+@app.route('/weather-report', methods = ['GET'])
 def retrieve_report():
-    return '<h1>This is where you can read the weather report(s)</h1>'
+    return '<h1>This is where you can read ALL the weather report(s)</h1>'
 
 #This route will allow the user to first view their reports, then update OR delete a report that has already been recorded and stored
-@app.route('/update-report')
-def update_report():
-    return '<h1>This allows you to update </h1>'
-def delete_report():
-    return '<h1>and delete the reports as desired</h1>'
+@app.route('/update-report/<int:id>', methods = ['GET', 'PUT'])
+def update_report(id):
+    return f'<h1>This allows you to view then update a certain weather report: {id} </h1>'
+
+@app.route('/update-report', methods = ['GET', 'DELETE'])
+def delete_report(id):
+    return f'<h1>and this allows you to view then delete a certain weather report as desired {id}</h1>'
 
 #This will allow you to download a copy of the report(s)
 @app.route('/download-report')
