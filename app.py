cf3c147db110842bcdd771a25681603a56cdc02d
vramos29 <vramos1@northpark.edu>
2026-03-17 19:35:28 -0500

    fixed CRUD routes

--- app.py
+++ app.py
@@ -14,29 +14,37 @@ app = Flask(__name__)
 def home():  
     return '<h1>This is the weather report app starter page</h1>'
 
-#This will be the post route that allows the user to enter the location for the weather report
-@app.route('/weather', methods = ['POST'])
+#Fetches live weather, saves it, returns record
+@app.route('/ingest?city=city&country=country', methods = ['POST'])
 def enter_request():
     return '<h1>This is where you can generate a weather report</h1>'
 
-#After entering their request, this is the get route that will display the information requested
-@app.route('/weather-report', methods = ['GET'])
+#Retrieves all stored observations
+@app.route('/observations', methods = ['GET'])
 def retrieve_report():
     return '<h1>This is where you can read ALL the weather report(s)</h1>'
 
-#This route will allow the user to first view their reports, then update OR delete a report that has already been recorded and stored
-@app.route('/update-report/<int:id>', methods = ['GET', 'PUT'])
+#Retrieves a specific obervation by ID
+@app.route('/observations/<int:id>', methods = ['GET'])
 def update_report(id):
-    return f'<h1>This allows you to view then update a certain weather report: {id} </h1>'
+    return f'<h1>This allows you to view a certain weather report: {id} </h1>'
 
-@app.route('/update-report', methods = ['GET', 'DELETE'])
-def delete_report(id):
-    return f'<h1>and this allows you to view then delete a certain weather report as desired {id}</h1>'
+#Updates a previously made observation
+@app.route('/observations/<int:id>', methods = ['PUT'])
+def update_report(id):
+    return f'<h1>This allows you to update a certain weather report: {id} </h1>'
 
+#Deletes an observation from the Database
+@app.route('/observations/<int:id>', methods = ['DELETE'])
+def update_report(id):
+    return f'<h1>This allows you to delete a certain weather report: {id} </h1>'
+
+"""
 #This will allow you to download a copy of the report(s)
 @app.route('/download-report')
 def download_report():
     return '<h1>This will allow you to download a copy of the report</h1>'
+"""
 
 # This ensures the server only runs if the script is executed directly - returns TypeError.
 if __name__ == '__main__':
